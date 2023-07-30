fdbc3f60680f06fc60586b2405ee97c8e562a995
Ismael-Mazlin <Ismael_Mazlin@outlook.fr>
2023-07-30 10:38:59 +0200

    isnegative.go

---
+++ quest2/isnegative.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func Isnegative(nbr int) {
+	if nbr < 0{
+		z01.PrintRune('T')
+	} else{
+		z01.PrintRune('F')
+	}
+}
+
+func main() {
+	Isnegative()
+}
